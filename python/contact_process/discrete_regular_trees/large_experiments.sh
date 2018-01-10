67670124097f88d1fa68368df51ff4229e8bc97a
mitchell <mitchellwortsman@gmail.com>
2018-01-10 16:50:32 -0500

    parallel large

--- python/contact_process/discrete_regular_trees/large_experiments.sh
+++ python/contact_process/discrete_regular_trees/large_experiments.sh
@@ -4,6 +4,6 @@ for d in 2 3 4 5 6
 do
   for T in 2 3 4 5 6
   do
-    python compare.py --steps 10000000 -d $d -T $T --iters 6 --epsilon 0.005
+    python compare.py --steps 10000000 -d $d -T $T --iters 6 --epsilon 0.005 &
   done
 done
